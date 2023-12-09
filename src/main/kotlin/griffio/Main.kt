414e0b6460b5ac011511923623e7a057867e0275
griff і⊙ <346896+griffio@users.noreply.github.com>
2023-12-09 19:30:05 +0000

    Update Main.kt

--- src/main/kotlin/griffio/Main.kt
+++ src/main/kotlin/griffio/Main.kt
@@ -3,7 +3,9 @@ package griffio
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.jdbc.asJdbcDriver
 import griffio.migrations.Address
+import griffio.migrations.City
 import griffio.migrations.Customer
+import griffio.migrations.Supplier
 import griffio.queries.Sample
 
 import org.postgresql.ds.PGSimpleDataSource
@@ -15,15 +17,22 @@ private fun getSqlDriver(): SqlDriver {
     return datasource.asJdbcDriver()
 }
 
+fun stringIdentifier(n: Int) = (1..n).map { ('A'..'Z').random() }.joinToString("")
+fun longIdentifier(n: Int) = (1..n).map { (1..10).random() }.joinToString("").toLong()
+fun phone() = "${stringIdentifier(3)}-${stringIdentifier(3)}-${stringIdentifier(3)}"
+fun postal() = "${stringIdentifier(6)}"
+
 fun main() {
 
     val driver = getSqlDriver()
     val sample = Sample(driver)
-    val city = sample.cityQueries.insert("City Name 1").executeAsOne()
-    val addressId = sample.addressQueries.insert("Address 1", "Address 2", "District 31", city.city_id, "513-281-4700", "POST CODE").executeAsOne()
+
+    val city = sample.cityQueries.insert("City Name ${stringIdentifier(10)}").executeAsOne()
+    val addressId = sample.addressQueries.insert("Address ${stringIdentifier(5)}", "Address 2", "District 31", city.city_id, phone(), postal()).executeAsOne()
     val customerId = sample.customerQueries.insert("First Name", "Last Name", "test@example.com", addressId, true).executeAsOne()
     val address: Address = sample.addressQueries.get(addressId).executeAsOne()
     val customer: Customer = sample.customerQueries.get(customerId).executeAsOne()
+    val supplier = sample.supplierQueries.insert(Supplier(longIdentifier(10), postal())).executeAsOne()
 
     // update by single address object not supported, have to provide fields
     sample.addressQueries.update(address_id = address.address_id,
@@ -33,6 +42,7 @@ fun main() {
     sample.customerQueries.update(customer_id = customer.customer_id, first_name = "First Name Updated", last_name = customer.last_name,
         email = customer.email, address_id = customer.address_id, is_active = customer.is_active)
 
+
     val addressList: List<Address> = sample.addressQueries.all().executeAsList()
     val customerList: List<Customer> = sample.customerQueries.all().executeAsList()
 
@@ -40,4 +50,5 @@ fun main() {
     println(address)
     println(customerList.joinToString("\n"))
     println(customer)
+    println(supplier)
 }
